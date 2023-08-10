e632b91afb9dc41716eb510c4168ae282f0e7dfa
ppgo8 <1159316682@qq.com>
2023-08-10 17:42:26 +0800

    完成了富文本和markdown功能的开发

--- 代码/my-app/src/views/PageOne.vue
+++ 代码/my-app/src/views/PageOne.vue
@@ -1,10 +1,64 @@
 <template>
-  <div>我是第一个页面</div>
+  <div>
+    <h1 class="plugins-tips">
+      wangEditor富文本编辑器 访问地址：<a
+        href="https://www.wangeditor.com/v5/for-frame.html"
+        target="_blank"
+        >wangeEitor</a
+      >
+    </h1>
+    <div style="border: 1px solid #ccc">
+      <Toolbar
+        style="border-bottom: 1px solid #ccc"
+        :editor="editor"
+        :defaultConfig="toolbarConfig"
+        :mode="mode"
+      />
+      <Editor
+        style="height: 55vh; overflow-y: hidden"
+        v-model="html"
+        :defaultConfig="editorConfig"
+        :mode="mode"
+        @onCreated="onCreated"
+      />
+    </div>
+    <el-button type="primary" style="margin-top: 10px" @click="save"
+      >保存</el-button
+    >
+  </div>
 </template>
 
 <script>
-export default {};
+import { Editor, Toolbar } from "@wangeditor/editor-for-vue";
+export default {
+  components: { Editor, Toolbar },
+  data() {
+    return {
+      editor: null,
+      html: "<p>hello</p>",
+      toolbarConfig: {},
+      editorConfig: { placeholder: "请输入内容..." },
+      mode: "default", // or 'simple'
+    };
+  },
+  methods: {
+    onCreated(editor) {
+      this.editor = Object.seal(editor); // 一定要用 Object.seal() ，否则会报错
+    },
+    // 用户点击保存
+    save() {
+      console.log(this.content);
+      console.log(this.html);
+      this.$message.success("保存成功！");
+    },
+  },
+  beforeDestroy() {
+    const editor = this.editor;
+    if (editor == null) return;
+    editor.destroy(); // 组件销毁时，及时销毁编辑器
+  },
+};
 </script>
 
-<style>
-</style>
+<style src="@wangeditor/editor/dist/css/style.css"></style>
+
--- 代码/my-app/src/views/PageTwo.vue
+++ 代码/my-app/src/views/PageTwo.vue
@@ -1,10 +1,38 @@
 <template>
-  <div>我是第二个页面</div>
+  <div>
+    <h1 class="plugins-tips">
+      Markdonw编辑器v-md-editor 访问地址：<a
+        href="https://imzbf.github.io/md-editor-v3/en-US/index"
+        target="_blank"
+        >v-md-editor</a
+      >
+    </h1>
+    <v-md-editor v-model="text" style="min-height: 60vh"></v-md-editor>
+    <el-button type="primary" style="margin-top: 10px" @click="save"
+      >保存</el-button
+    >
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      text: "",
+    };
+  },
+  methods: {
+    // 用户点击保存
+    save() {
+      console.log(this.text);
+      this.$message.success("保存成功！");
+    },
+  },
+};
 </script>
 
 <style>
+.v-md-editor {
+  overflow: hidden;
+}
 </style>
